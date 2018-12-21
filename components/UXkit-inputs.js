a87f9e7372ce253ba9b85b6d053f822c5c33c24b
ofirl <ofirl24@gmail.com>
2018-12-21 23:49:28 +0200

    starting to add inputs

---
+++ components/UXkit-inputs.js
@@ -0,0 +1,19 @@
+function UXkitInitInputs() {
+    var floatingInputs = document.querySelectorAll(".floating-input > input");
+    for (let i = 0; i < floatingInputs.length; i++) {
+        const input = floatingInputs[i];
+        
+        input.addEventListener("change", function (e) {
+
+        });
+        input.addEventListener("keydown", function (e) {
+
+        });
+    }
+}
+
+function checkFloatingInput() {
+    
+}
+
+UXkitInitInputs();
