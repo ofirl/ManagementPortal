8bb9ae98502d379308956c80db9bbf473966e66c
ofirl <ofirl24@gmail.com>
2018-12-22 13:05:10 +0200

    floating labal input fix

--- components/UXkit-inputs.js
+++ components/UXkit-inputs.js
@@ -1,22 +1,22 @@
+const inputTagName = 'INPUT';
+const floatingInputClassName = 'floating-input';
+
 function UXkitInitInputs() {
-    var floatingInputs = document.querySelectorAll(".floating-input > input");
-    for (let i = 0; i < floatingInputs.length; i++) {
-        const input = floatingInputs[i];
-        
-        // input.addEventListener("change", function (e) {
-        //     checkFloatingInput(e.target);
-        // });
-        input.addEventListener("input", function (e) {
-            checkFloatingInput(e.target);
-        });
-    }
+    document.body.addEventListener("input", function (e) {
+        checkFloatingInput(e.target);
+    });
 }
 
-function checkFloatingInput(inputElement) {
-    if (inputElement.value != "")
-        document.querySelector("[for='" + inputElement.id + "'").classList.add("floating-input-label-float");
+function checkFloatingInput(target) {
+    if (target.tagName != inputTagName)
+        return;
+    if (!target.parentElement.classList.contains(floatingInputClassName))
+        return;
+
+    if (target.value != "")
+        document.querySelector("[for='" + target.id + "'").classList.add("floating-input-label-float");
     else
-        document.querySelector("[for='" + inputElement.id + "'").classList.remove("floating-input-label-float");
+        document.querySelector("[for='" + target.id + "'").classList.remove("floating-input-label-float");
 }
 
 window.addEventListener("load", UXkitInitInputs);
