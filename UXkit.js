a0c73b1f6747daf3b5f0af0377357ed37bb0c783
ofirl <ofirl24@gmail.com>
2018-12-25 01:45:13 +0200

    added documentation

--- UXkit.js
+++ UXkit.js
@@ -1,102 +1,209 @@
 /* General Helping Functions -- General Helping Functions -- General Helping Functions -- General Helping Functions -- General Helping Functions */
 
+/** @class Element */
+
 /**
- * Toggles a class on a DOM element
- * @param {Element} element 
- * @param {String} className 
+ * Helper Functions
+ * @namespace
  */
-function toggleElementClass(element, className) {
-    element.toggleClass();
-    if (element.classList.contains(className))
-        element.classList.remove(className);
-    else
-        element.classList.add(className);
+var helperFuncs = {
+    /**
+     * Toggles a class on a DOM element
+     * @param {Element} element element to change
+     * @param {String} className class to toggle
+     */
+    toggleElementClass : function (element, className) {
+        element.toggleClass();
+        if (element.classList.contains(className))
+            element.classList.remove(className);
+        else
+            element.classList.add(className);
+    },
+    /**
+     * Checks an element with a predefined test
+     * @param {Element} element element to check
+     * @param {string} test test to run
+     * @param {*} value value to check against
+     * @returns {boolean}
+     */
+    testElement : function (element, test, value) {
+        var testDef = elementTests[test];
+        return checkElementProperty(element, testDef["properties"], testFunctions[testDef["function"]], value);
+    },
+    /**
+     * Checks an element property
+     * @param {Element} element element to check
+     * @param {string[]} properties element properties to follow
+     * @param {testFunction} testFunc function to check with
+     * @param {*} value value to check against
+     * @returns {boolean}
+     */
+    checkElementProperty : function (element, properties, testFunc, value) {
+        var current = element;
+        for (let i = 0; i < properties.length; i++) {
+            const property = properties[i];
+            
+            current = current[property];
+        }
+
+        return testFunc(current, value);
+    },
+    /**
+     * Checks an element tag
+     * @param {Element} element element to check
+     * @param {string} tag tag to check against
+     * @returns {boolean}
+     */
+    checkElementTag : function (element, tag) {
+        return testElement(element, testNames.elementTagTest, tag);
+    },
+    /**
+     * Checks an element parent class
+     * @param {Element} element element to check
+     * @param {string} parentClass class to check against
+     * @returns {boolean}
+     */
+    checkElementParentClass : function (element, parentClass) {
+        return testElement(element, testNames.parentElementClassTest, parentClass);
+    },
+    /**
+     * Checks an element class
+     * @param {Element} element element to check
+     * @param {string} elementClass class to check against
+     * @returns {boolean}
+     */
+    checkElementClass : function (element, elementClass) {
+        return testElement(element, testNames.elementClassTest, elementClass);
+    }
 }
 
 /**
  * Toggles a class on a DOM element
- * @param {String} className 
+ * @param {String} className class to toggle
  * @memberof Element
- * @method
- * @name Element#toggleClass
  */
 Element.prototype.toggleClass = function (className) {
-    return toggleElementClass(this, className);
+    return helperFuncs.toggleElementClass(this, className);
 };
 
 /* General Helping Functions -- General Helping Functions -- General Helping Functions -- General Helping Functions -- General Helping Functions */
 
 /* Element Tests -- Element Tests -- Element Tests -- Element Tests -- Element Tests -- Element Tests -- Element Tests -- Element Tests -- Element Tests -- Element Tests */
 
-/* Pre defined test names */
-const elementTagTest = "tag";
-const elementClassTest = "class";
-const parentElementClassTest = "parentClass";
+/**
+ * Pre defined test names
+ * @namespace
+ */
+var testNames = {
+    /** @constant 
+        @type {string} test name for element tag test
+        @default
+    */
+    elementTagTest : "tag",
+    /** @constant 
+        @type {string} test name for element class test
+        @default
+    */
+    elementClassTest : "class",
+    /** @constant 
+        @type {string} test name for element parent class test
+        @default
+    */
+    parentElementClassTest : "parentClass"
+}
 
-/* Pre defined tests */
+/** 
+ * Pre defined tests
+ * @namespace
+ */
 const elementTests = {
-    "tag" : { 
+    /**
+     * @typedef {object} ElementTest
+     * @property {string[]} properties - properties array.
+     * @property {string} function - condition function.
+     * @memberof elementTests
+     */
+    /** @constant 
+     *  @type {ElementTest} test for element tag
+     */
+    elementTagTest : { 
         "properties" : ["tagName"],
         "function" : "equal"
     },
-    "parentClass" : {
+    /** @constant 
+     *  @type {ElementTest} test for element parent class
+     */
+    parentElementClassTest : {
         "properties" : ["parentElement", "classList"],
         "function" : "contains"
     },
-    "class" : {
+    /** @constant 
+     *  @type {ElementTest} test for element class
+     */
+    elementClassTest : {
         "properties" : ["classList"],
         "function" : "contains"
     }
 };
 
-/* Pre defined test functions */
+/** 
+ * Pre defined test functions 
+ * @namespace
+ */
 const testFunctions = {
+    /** 
+     *  a test function
+     *  @typedef {function} testFunction
+     *  @param {*} current value to check
+     *  @param {*} valuevalue to check against
+     *  @memberof testFunctions
+     */
+    /**
+     * Checks for equality (==)
+     * @param {*} current value
+     * @param {*} value value to check against
+     */
     "equal" : function (current, value) { return current == value; },
-    "=" : function (current, value) { return current == value; },
+    /**
+     * Checks for identity (===)
+     * @param {*} current value
+     * @param {*} value value to check against
+     */
+    "identity" : function (current, value) { return current === value; },
+    /**
+     * Checks for contains (for arrays)
+     * @param {Array.<*>} list array
+     * @param {*} value value to search for in the array
+     */
     "contains" : function (list, value) { return list.contains(value); }
 }
 
-/* helper function */
-function testElement(element, test, value) {
-    var testDef = elementTests[test];
-    return checkElementProperty(element, testDef["properties"], testFunctions[testDef["function"]], value);
-}
-
-/* property check */
-function checkElementProperty(element, properties, testFunc, value) {
-    var current = element;
-    for (let i = 0; i < properties.length; i++) {
-        const property = properties[i];
-        
-        current = current[property];
-    }
-
-    return testFunc(current, value);
-}
-
 /* shorthand for predefined tests */
-function checkElementTag(element, tag) {
-    return testElement(element, elementTagTest, tag);
-}
-
+/**
+ * Checks an element tag
+ * @param {String} tag tag to check against
+ * @memberof Element
+ */
 Element.prototype.checkElementTag = function (tag) {
-    return testElement(this, elementTagTest, tag);
+    return helperFuncs.checkElementTag(this, tag);
 }
 
-function checkElementParentClass(element, parentClass) {
-    return testElement(element, parentElementClassTest, parentClass);
-}
-
+/**
+ * Checks an element parent class
+ * @param {String} parentClass class to check against
+ * @memberof Element
+ */
 Element.prototype.checkElementParentClass = function (parentClass) {
-    return testElement(this, parentElementClassTest, parentClass);
+    return helperFuncs.checkElementParentClass(this, parentClass);
 }
 
-function checkElementClass(element, elementClass) {
-    return testElement(element, elementClassTest, elementClass);
-}
-
+/**
+ * Checks an element class
+ * @param {String} elementClass class to check against
+ * @memberof Element
+ */
 Element.prototype.checkElementClass = function (elementClass) {
-    return testElement(this, elementClassTest, elementClass);
+    return helperFuncs.checkElementClass(this, elementClass);
 }
 
 /* Element Tests -- Element Tests -- Element Tests -- Element Tests -- Element Tests -- Element Tests -- Element Tests -- Element Tests -- Element Tests -- Element Tests */
